35381963995308f08855104dc7c77820102381b7
Thai Pham <pham.tha@northeastern.edu>
2022-04-07 02:48:45 -0400

    refractoring

--- refractor.py
+++ refractor.py
@@ -1,4 +1,3 @@
-
 import socket
 import sys
 import time
@@ -7,17 +6,10 @@ from urlparse import urlparse
 from random import randint
 from struct import pack, unpack, calcsize
 
-
-
 REMOTE_PORT = 80
 LOCAL_PORT = randint(1001, 65535)
 LOCAL_HOST = ''
 REMOTE_HOST = ''
-
-IPDatagram = namedtuple(
-    'IPDatagram', 'ip_tlen ip_id ip_frag_off ip_saddr ip_daddr data ip_check')
-TCPSeg = namedtuple(
-    'TCPSeg', 'tcp_source tcp_dest tcp_seq tcp_ack_seq tcp_check data tcp_flags tcp_adwind')
 
 # Flags
 FLAGS = {'SYN': 2, 'ACK': 16, 'PSH_ACK': 24, 'FIN': 1, 'FIN_ACK': 17}
@@ -28,8 +20,7 @@ send_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 receive_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 tcp_sequence = 0
 tcp_ack_sequence = 0
-send_buffer = ''
-
+data_length = 0
 
 
 # TODO: Close connection
@@ -38,15 +29,8 @@ send_buffer = ''
 # https://wiki.wireshark.org/TCP-4-times-close.md
 
 class TCP_Response(object):
-    # tcp_src = ''
-    # tcp_dst = ''
-    # tcp_sequence = 0
-    # tcp_ack_sequence = 0
-    # tcp_check = 0
-    # tcp_flag =
-    # tcp_data =
-
-    def __init__(self, tcp_src, tcp_dst, tcp_sequence, tcp_ack_sequence, tcp_data_offset, tcp_check, tcp_flag, tcp_data):
+    def __init__(self, tcp_src, tcp_dst, tcp_sequence, tcp_ack_sequence, tcp_data_offset, tcp_check, tcp_flag,
+                 tcp_data):
         self.tcp_src = tcp_src
         self.tcp_dst = tcp_dst
         self.tcp_sequence = tcp_sequence
@@ -56,16 +40,6 @@ class TCP_Response(object):
         self.tcp_flag = tcp_flag
         self.tcp_data = tcp_data
 
-
-# class IP_Response(object):
-#     ip_src = ''
-#     ip_dst = ''
-#     ip_check = 0
-#
-#     def __init__(self, ip_src, ip_dst, ip_check):
-#         self.ip_src = ip_src
-#         self.ip_dst = ip_dst
-#         self.ip_check = ip_check
 
 # checksum functions needed for calculation checksum
 # TODO: REFACTOR TO AVOID PLAGIARISM? KB DC CHUA
@@ -81,26 +55,12 @@ def checksum(message):
     _sum = ~_sum & 0xffff
     return _sum
 
-    # s = 0
-    # for i in range(0, len(message), 2):
-    #     if i + 1 <= len(message) - 1:
-    #         w = ord(message[i]) + (ord(message[i + 1]) << 8)
-    #     else:
-    #         w = ord(message[i])
-    #
-    #     s = s + w
-    #
-    # s = (s >> 16) + (s & 0xffff)
-    # s = s + (s >> 16)
-    # s = ~s & 0xffff
-    #
-    # return s
 
-#https://www.delftstack.com/howto/python/get-ip-address-python/
+# https://www.delftstack.com/howto/python/get-ip-address-python/
 def extract_addr(host):
-    #src_addr = socket.gethostbyname(socket.gethostname())
-    src_addr = '172.23.214.109' #TODO
-    #print host
+    # src_addr = socket.gethostbyname(socket.gethostname())
+    src_addr = '172.23.214.109'  # TODO
+    # print host
     dst_addr = socket.gethostbyname(host)
     return src_addr, dst_addr
 
@@ -113,7 +73,6 @@ def create_tcp_header(data, flags):
     tcp_dest = REMOTE_PORT  # destination port
     tcp_seq = tcp_sequence
     tcp_ack_seq = tcp_ack_sequence
-    # print "tcp_ack_seq in tcp", tcp_ack_sequence
     tcp_doff = 5  # 4 bit field, size of tcp header, 5 * 4 = 20 bytes
     tcp_window = socket.htons(2048)  # maximum allowed window size
     tcp_check = 0
@@ -136,13 +95,13 @@ def create_tcp_header(data, flags):
     psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length)
     psh = psh + tcp_header + data
 
-
     tcp_check = checksum(psh)
 
     # make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
     tcp_header = pack('!HHLLBBH', tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,
                       tcp_window) + pack('H', tcp_check) + pack('!H', tcp_urg_ptr)
     return tcp_header
+
 
 # get from tutorial,
 # TODO: REFACTOR TO AVOID PLAGIARISM
@@ -188,23 +147,16 @@ def create_packet(data, flags):
 
 def filter_tcp_response(received_tcp_response):
     if not received_tcp_response:
-        #print "empty"
         return False
-    #print "response", received_tcp_response
     received_tcp_flags = received_tcp_response.tcp_flag
-    #print "flag", received_tcp_flags
     received_tcp_ack_sequence = received_tcp_response.tcp_ack_sequence
-    #print "ack_sequence", received_tcp_ack_sequence
-    #print "tcp_sequence", tcp_sequence
     if (not (received_tcp_flags & FLAGS['ACK'])) or received_tcp_ack_sequence < tcp_sequence + 1:
-        #print "not tcp acked"
         return False
 
-    #sys.exit(1)
     return True
 
 
-#TODO: done
+# TODO: done
 # https://www.cs.miami.edu/home/burt/learning/Csc524.092/notes/ip_example.html
 def get_ip_response(received_packet):
     # Get IP header list
@@ -221,7 +173,6 @@ def get_ip_response(received_packet):
         ip_header_size += opts_size
     # Calculate IP header size
 
-
     # Retrieve IP data
     ip_data = received_packet[ip_header_size:ip_header_list[2]]
 
@@ -229,32 +180,10 @@ def get_ip_response(received_packet):
     ip_header = received_packet[:ip_header_size]
     ip_check_sum = checksum(ip_header)
 
-
     return ip_src, ip_dst, ip_data, ip_check_sum
 
-    # hdr_fields = unpack('!BBHHHBBH4s4s', received_packet[:20])
-    # ip_header_size = calcsize('!BBHHHBBH4s4s')
-    # ip_ver_ihl = hdr_fields[0]
-    # ip_ihl = ip_ver_ihl - (4 << 4)
-    #
-    # if ip_ihl > 5:
-    #     opts_size = (ip_ihl - 5) * 4
-    #     ip_header_size += opts_size
-    #
-    # ip_headers = received_packet[:ip_header_size]
-    #
-    # data = received_packet[ip_header_size:hdr_fields[2]]
-    # ip_check = checksum(ip_headers)
-    #
-    # return IPDatagram(ip_daddr=socket.inet_ntoa(hdr_fields[-1]),
-    #                   ip_saddr=socket.inet_ntoa(hdr_fields[-2]),
-    #                   ip_frag_off=hdr_fields[4],
-    #                   ip_id=hdr_fields[3],
-    #                   ip_tlen=hdr_fields[2],
-    #                   ip_check=ip_check, data=data)
 
-
-#TODO
+# TODO
 # https://www.quora.com/What-is-TCP-checksum
 # https://www.oreilly.com/library/view/internet-core-protocols/1565925726/re69.html
 # https://en.wikipedia.org/wiki/Transmission_Control_Protocol
@@ -287,35 +216,6 @@ def get_tcp_response(ip_data):
     # Return TCP_Response object
     return TCP_Response(tcp_src, tcp_dst, tcp_seq, tcp_ack_seq, tcp_data_offset, tcp_check, tcp_flag, tcp_data)
 
-    # tcp_header_size = calcsize('!HHLLBBHHH')
-    # hdr_fields = unpack('!HHLLBBHHH', ip_data[:tcp_header_size])
-    # tcp_source = hdr_fields[0]
-    # tcp_dest = hdr_fields[1]
-    # tcp_seq = hdr_fields[2]
-    # tcp_ack_seq = hdr_fields[3]
-    # tcp_doff_resvd = hdr_fields[4]
-    # tcp_doff = tcp_doff_resvd >> 4  # get the data offset
-    # tcp_adwind = hdr_fields[6]
-    # tcp_urg_ptr = hdr_fields[7]
-    # # parse TCP flags
-    # tcp_flags = hdr_fields[5]
-    # # process the TCP options if there are
-    # # currently just skip it
-    # if tcp_doff > 5:
-    #     opts_size = (tcp_doff - 5) * 4
-    #     tcp_header_size += opts_size
-    # # get the TCP data
-    # data = ip_data[tcp_header_size:]
-    # # compute the checksum of the recv packet with psh
-    # tcp_check = _tcp_check(ip_data)
-    # # tcp_check = 0
-    # return TCPSeg(tcp_seq=tcp_seq,
-    #               tcp_source=tcp_source,
-    #               tcp_dest=tcp_dest,
-    #               tcp_ack_seq=tcp_ack_seq,
-    #               tcp_adwind=tcp_adwind,
-    #               tcp_flags=tcp_flags, tcp_check=tcp_check, data=ip_data[tcp_header_size:])
-
 
 def _tcp_check(payload):
     # pseudo header fields
@@ -326,11 +226,10 @@ def _tcp_check(payload):
     tcp_length = len(payload)
 
     psh = pack('!4s4sBBH', source_address, dest_address,
-                      placeholder, protocol, tcp_length)
+               placeholder, protocol, tcp_length)
     psh = psh + payload
 
     return checksum(psh)
-
 
 
 def is_valid_tcp_response(received_tcp_response):
@@ -343,86 +242,55 @@ def is_valid_ip_response(ip_src, ip_dst, ip_checksum):
 
 def receive_tcp():
     receive_socket.settimeout(60)
-    #print "start receive"
-    #print tcp_sequence
     try:
         while True:
-            #print "in loop"
             received_packet = receive_socket.recv(MAX_SIZE)
-            #print "received packet"
             ip_src, ip_dst, ip_data, ip_checksum = get_ip_response(received_packet)
-            #print ip_src, ip_dst, ip_data, ip_checksum
             if is_valid_ip_response(ip_src, ip_dst, ip_checksum):
-                #print "valid ip"
                 received_tcp_response = get_tcp_response(ip_data)
                 if is_valid_tcp_response(received_tcp_response):
-                    #print "valid tcp"
-                    #print "receieve tcp!!"
                     return received_tcp_response
-
-            # data = receive_socket.recv(MAX_SIZE)
-            # ip_datagram = get_ip_response(data)
-            # if ip_datagram.ip_daddr != LOCAL_HOST or ip_datagram.ip_check != 0 or ip_datagram.ip_saddr != REMOTE_HOST:
-            #     continue
-            #
-            # tcp_seg = get_tcp_response(ip_datagram.data)
-            # #print tcp_seg.data
-            # if tcp_seg.tcp_source != REMOTE_PORT or tcp_seg.tcp_dest != LOCAL_PORT or tcp_seg.tcp_check != 0:
-            #     continue
-            # return tcp_seg
     except socket.timeout:
         print "Time out when getting packet"
         return None
 
-#nho sua
+
 def acked():
     global tcp_sequence, tcp_ack_sequence
     current_time = time.time()
-    # print tcp_sequence
     while time.time() - current_time < 10:
         received_tcp = receive_tcp()
         if filter_tcp_response(received_tcp):
-            # print "acked"
             tcp_sequence = received_tcp.tcp_ack_sequence
             tcp_ack_sequence = received_tcp.tcp_sequence + 1
             return True
 
     return False
-    # global tcp_sequence, tcp_ack_sequence
-    # start_time = time.time()
-    # while time.time() - start_time < 60:
-    #     tcp_seg = receive_tcp()
-    #     if not tcp_seg:
-    #         break
-    #     if tcp_seg.tcp_flags & FLAGS['ACK'] and tcp_seg.tcp_ack_seq >= tcp_sequence + 1:
-    #         tcp_sequence = tcp_seg.tcp_ack_seq
-    #         tcp_ack_sequence = tcp_seg.tcp_seq + 1
-    #         return True
-    #
-    # return False
+
 
 # TODO: https://accedian.com/blog/diagnose-tcp-connection-setup-issues/
-#Three-way handshake
-def established_connection(src_addr, dst_addr):
-    #print REMOTE_HOST, REMOTE_PORT
+# Three-way handshake
+def established_connection():
     global tcp_sequence
     tcp_sequence = randint(0, (2 << 31) - 1)
+    #print syn_packet
+
     syn_packet = create_packet('', FLAGS['SYN'])
+
     send_socket.sendto(syn_packet, (REMOTE_HOST, REMOTE_PORT))
 
     if not acked():
         print "Cannot ACK when establishing connection!"
         return False
 
-
     ack_packet = create_packet('', FLAGS['ACK'])
     send_socket.sendto(ack_packet, (REMOTE_HOST, REMOTE_PORT))
     print "Done 3way handshake!"
     return True
 
+
 def closed_connection():
     fin_ack_packet = create_packet('', FLAGS['FIN_ACK'])
-    ack_packet = create_packet('', FLAGS['ACK'])
 
     current_time = time.time()
 
@@ -432,6 +300,7 @@ def closed_connection():
         tcp_response = receive_tcp()
         flag = tcp_response.tcp_flag
         if flag & FLAGS['FIN']:
+            ack_packet = create_packet('', FLAGS['ACK'])
             send_socket.sendto(ack_packet, (REMOTE_HOST, REMOTE_PORT))
             send_socket.close()
             receive_socket.close()
@@ -441,13 +310,14 @@ def closed_connection():
     print "Closing connection failed!"
     return False
 
+
 # fuck
 # https://stackoverflow.com/questions/15182106/what-is-the-reason-and-how-to-avoid-the-fin-ack-rst-and-rst-ack
 def received():
-    global tcp_sequence, tcp_ack_sequence, buffer_length
+    global tcp_sequence, tcp_ack_sequence
     current_time = time.time()
 
-    #Time out after 60s for getting packet
+    # Time out after 60s for getting packet
     while time.time() - current_time < 60:
         received_tcp = receive_tcp()
         if not received_tcp:
@@ -455,7 +325,7 @@ def received():
             return False
         received_tcp_flags = received_tcp.tcp_flag
         received_tcp_ack_sequence = received_tcp.tcp_ack_sequence
-        if received_tcp_flags & FLAGS['ACK'] and received_tcp_ack_sequence >= tcp_sequence + len(send_buffer):
+        if received_tcp_flags & FLAGS['ACK'] and received_tcp_ack_sequence >= tcp_sequence + data_length:
             tcp_sequence = received_tcp.tcp_ack_sequence
             tcp_ack_sequence = received_tcp.tcp_sequence
             return True
@@ -464,9 +334,7 @@ def received():
 
 
 def send(packet):
-    global send_buffer
     current_time = time.time()
-    #print REMOTE_HOST, REMOTE_PORT
     send_socket.sendto(packet, (REMOTE_HOST, REMOTE_PORT))
     while not received():
         if time.time() - current_time > 180:
@@ -475,7 +343,6 @@ def send(packet):
                 sys.exit(1)
             sys.exit(0)
         send_socket.sendto(packet, (REMOTE_HOST, REMOTE_PORT))
-    send_buffer = ''
     print "Sent data"
 
 
@@ -490,20 +357,13 @@ def receive():
             # if not closed_connection():
             #     sys.exit(1)
             sys.exit(1)
-        #rint "receiving packets ..."
-        #print "here"
         packet_flags = packet.tcp_flag
         packet_tcp_sequence = packet.tcp_sequence
         packet_data = packet.tcp_data
-        #print "packet_flags ", packet_flags
-        #print "sequence ", packet_tcp_sequence
-        #print "data: ", packet_data
 
         if packet_flags & FLAGS['ACK'] and packet_tcp_sequence not in received_packets:
             received_packets[packet_tcp_sequence] = packet_data
             tcp_ack_sequence = packet_tcp_sequence + len(packet_data)
-            #print "putting data in ..."
-            #print received_packets
             if packet_flags & FLAGS['FIN']:
                 print "Finish!"
                 return received_packets
@@ -520,7 +380,6 @@ def receive():
 
 
 def is_valid(data):
-    #print data
     try:
         position = data.index("\r\n\r\n") + 4
     except:
@@ -556,20 +415,17 @@ def process_data(packets):
 
 
 def run(url):
-    global LOCAL_HOST, REMOTE_HOST, buffer_length, send_buffer
+    global LOCAL_HOST, REMOTE_HOST, data_length
 
     host, path = extract_url(url)
     src_addr, dst_addr = extract_addr(host)
     LOCAL_HOST = src_addr
     REMOTE_HOST = dst_addr
 
-    if established_connection(LOCAL_HOST, REMOTE_HOST):
+    if established_connection():
         request = "GET " + path + " HTTP/1.0\r\n" + "Host: " + host + "\r\n\r\n"
-        buffer_length = len(request)
-        send_buffer = request
+        data_length = len(request)
         packet = create_packet(request, FLAGS['PSH_ACK'])
-
-
         send(packet)
         packets = receive()
         data = process_data(packets)
@@ -579,8 +435,8 @@ def run(url):
         export_file(path, body)
     else:
         print "Failed to establish connection!"
-        #send_socket.close()
-        #receive_socket.close()
+        # send_socket.close()
+        # receive_socket.close()
         sys.exit(1)
 
 
@@ -594,15 +450,9 @@ def check_url(url):
 
     return url
 
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit("Invalid number of arguments")
     url = check_url(sys.argv[1])
-    #print url
     run(url)
-
-
-
-
-
-
